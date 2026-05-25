8f274de3e7d7e8375ba10eb341342911e5a2568c
bocaraluca <raluca.boca@stud.ubbcluj.ro>
2026-05-25 22:05:44 +0300

    Changed smtp tp use brevo

--- backend/src/lib/splitmates/services/auth/mail-service.ts
+++ backend/src/lib/splitmates/services/auth/mail-service.ts
@@ -2,7 +2,7 @@ import { prisma } from '@/lib/prisma';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: 'smtp-relay.brevo.com',
   port: 587,
   secure: false,
   auth: {
